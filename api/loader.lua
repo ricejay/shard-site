1cfcee6a56a8c35f79d2c2f812646d5c4dcf7a69
Rice Jay <earljhen.n@gmail.com>
2025-05-22 03:30:12 +0800

    Update loader.lua

--- api/loader.lua
+++ api/loader.lua
@@ -1,1 +1,8 @@
-print("test")
+local gameIds = {
+    [126884695634066] = "GrowAGarden"
+}
+
+local loadScript = gameIds[game.PlaceId]
+if loadScript then
+    loadstring(game:HttpGetAsync(loadScript))()
+end
